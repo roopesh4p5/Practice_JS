e030153c7d1559d3f467e661a566e7269f574384
LeetCode Practice App <noreply@leetcode-practice.com>
2025-06-16 18:00:54 +0530

    Create question file: factorial

---
+++ factorial.js
@@ -0,0 +1,11 @@
+// Question: factorial
+// Description: funtion to find factorial
+// Created on: 16/06/2025, 18:00:54
+
+// Write your solution here
+function solution() {
+    // Your code here
+}
+
+// Test your solution
+console.log(solution());
