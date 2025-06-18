ac8eee2eefe727584fbd1b7d74ac3fa31c498cee
LeetCode Practice App <noreply@leetcode-practice.com>
2025-06-18 08:29:59 +0530

    Create question file: even numbers

---
+++ evennumbers.js
@@ -0,0 +1,11 @@
+// Question: even numbers
+// Description: find the even numbers in the array
+// Created on: 18/06/2025, 08:29:58
+
+// Write your solution here
+function solution() {
+    // Your code here
+}
+
+// Test your solution
+console.log(solution());
