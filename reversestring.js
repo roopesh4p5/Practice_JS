1ccc7f4199df5a10eb6ed4d0da9f69227f836bb0
LeetCode Practice App <noreply@leetcode-practice.com>
2025-06-18 08:23:38 +0530

    Add new approach for: reverse string

--- reversestring.js
+++ reversestring.js
@@ -12,3 +12,20 @@ function solution() {
 
 // Test your solution
 console.log(solution());
+
+================================================================================
+// NEW SOLUTION APPROACH - Added on: 18/06/2025, 08:23:38
+// Question: reverse string
+// Description: revese a sentence maintaining the words on the same place
+example
+pace wisdom solution
+ecap modsiw noitulos
+// Created on: 18/06/2025, 08:03:11
+
+// Write your solution here
+function solution() {
+    // Your code here
+}
+
+// Test your solution
+console.log(solution());
