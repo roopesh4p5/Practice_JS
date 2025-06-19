b10527a1a922e1fc22298b90d32b770c9af49027
LeetCode Practice App <noreply@leetcode-practice.com>
2025-06-19 16:06:03 +0530

    Create question file: factorial

---
+++ factorial.js
@@ -0,0 +1,11 @@
+// Question: factorial
+// Description: factorial funtion to get factorial of number
+// Created on: 19/06/2025, 16:06:02
+
+// Write your solution here
+function solution() {
+    // Your code here
+}
+
+// Test your solution
+console.log(solution());
