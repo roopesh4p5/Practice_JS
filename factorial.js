065ed981c7603757347c34de4754f434db686e89
LeetCode Practice App <noreply@leetcode-practice.com>
2025-06-19 16:57:37 +0530

    Add new approach for: factorial

--- factorial.js
+++ factorial.js
@@ -9,3 +9,15 @@ function solution() {
 
 // Test your solution
 console.log(solution());
+
+================================================================================
+// NEW SOLUTION APPROACH - Added on: 19/06/2025, 16:57:37
+const factorialOfNumber=(n)=>{
+  let res = 1;
+  for(let i=1;i<=n;i++){
+    res *= i;
+  }
+  return res;
+}
+
+console.log(factorialOfNumber(5))
