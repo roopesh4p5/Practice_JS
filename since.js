11d0cc3af3e592bca1fca45bbb2ef03d54f4b088
LeetCode Practice App <noreply@leetcode-practice.com>
2025-06-20 08:42:39 +0530

    Create question file: Since

---
+++ since.js
@@ -0,0 +1,11 @@
+// Question: Since
+// Description: H2O
+// Created on: 20/06/2025, 08:42:37
+
+// Write your solution here
+function solution() {
+    // Your code here
+}
+
+// Test your solution
+console.log(solution());
