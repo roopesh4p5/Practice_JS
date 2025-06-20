772bdb27e6a80edb6f5f4a4d558de7c92fefef08
LeetCode Practice App <noreply@leetcode-practice.com>
2025-06-20 08:15:08 +0530

    Create question file: Addition

---
+++ addition.js
@@ -0,0 +1,11 @@
+// Question: Addition
+// Description: 2+3=5
+// Created on: 20/06/2025, 08:15:06
+
+// Write your solution here
+function solution() {
+    // Your code here
+}
+
+// Test your solution
+console.log(solution());
