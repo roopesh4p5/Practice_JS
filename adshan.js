6e7cc4a83ded139b5a5f734b8ca6755a8af43fb0
LeetCode Practice App <noreply@leetcode-practice.com>
2025-06-20 08:17:09 +0530

    Create question file: Adshan

---
+++ adshan.js
@@ -0,0 +1,11 @@
+// Question: Adshan
+// Description: 25+13=38
+// Created on: 20/06/2025, 08:17:07
+
+// Write your solution here
+function solution() {
+    // Your code here
+}
+
+// Test your solution
+console.log(solution());
